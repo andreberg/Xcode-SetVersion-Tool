e60949b9d64cf29ea23256edaec34740a368661f
André Berg <andre.bergmedia@googlemail.com>
2010-08-29 20:58:55 +0200

    Add a little comment as to what version.sh is about

--- version.sh
+++ version.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
+# Shows a possibility how setversion can be used to update Info.plist based on SVN revision data
+
 SVN="false"
 
-if [[ ! -e Versioning/setversion ]]; then
-    gcc -o Versioning/setversion -w -framework Foundation -framework CoreServices Versioning/setversion.m
+if [[ ! -e setversion ]]; then
+    gcc -o setversion -w -framework Foundation -framework CoreServices setversion.m
 fi
 
 if [[ $SVN == "false" ]]; then
-   Versioning/setversion Info.plist
+   setversion Info.plist
 else
    REV="`svn info | grep Revision | cut -f 2 -d ':' | cut -f 2 -d ' '`"
-   Versioning/setversion Info.plist "" $REV
+   setversion Info.plist "" $REV
 fi
